09800a713ac9d398b6595f4ca825736b5d29c41f
Alex Eliasmith <alex@eliasmiths.com>
2018-04-07 14:51:21 -0400

    Moving P
    It moves

--- app.js
+++ app.js
@@ -11,14 +11,13 @@ serv.listen(2000); // Server port localhost:2000
 console.log('Server started.');
 
 var SOCKET_LIST = {};
-
 var io = require('socket.io')(serv,{});//listens when someone connects
 io.sockets.on('connection',function(socket){
 	socket.id = Math.random();
 	socket.x = 0;
 	socket.y = 0;
 	SOCKET_LIST[socket.id] = socket;
-	console.log('socket connection') 
+	console.log('socket connection');
 	//socket.on('happy',function(data){ //listens for message happy
 	//	console.log('happy because' + data.reason);
 	//});
@@ -27,3 +26,22 @@ io.sockets.on('connection',function(socket){
 	//	msg:'hello',
 	//});
 });
+
+setInterval(function(){
+	var pack = [];
+	for(var i in SOCKET_LIST){ //Loop through all players
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y
+		});
+	}
+	for(var i in SOCKET_LIST){ //Loop through all players
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions',pack); 
+	}
+
+	
+},1000/25);
